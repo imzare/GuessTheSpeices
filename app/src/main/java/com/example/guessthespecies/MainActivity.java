b736c03e61252b619447c286ef25831e0ee9c625
Hassan001 <bitf19a001@pucit.edu.pk>
2022-11-10 21:58:53 +0500

    Create random specie generator function from the list of given names in the declared arrays

--- app/src/main/java/com/example/guessthespecies/MainActivity.java
+++ app/src/main/java/com/example/guessthespecies/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.guessthespecies;
-import android.graphics.Color;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -20,6 +19,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             birdCorrectCount, birdWrongCount, insectCorrectCount, insectWrongCount;
     String answer = "";
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.organismName);
+        btnOfAnimal = findViewById(R.id.btnAnimal);
+        btnOfAnimal.setOnClickListener(this);
+        btnOfBird = findViewById(R.id.btnBird);
+        btnOfBird.setOnClickListener(this);
+        btnOfInsect = findViewById(R.id.btnInsect);
+        btnOfInsect.setOnClickListener(this);
+        textViewAnswer = findViewById(R.id.textViewResult);
+        textViewAnimalInformation = findViewById(R.id.textViewResultAnimal);
+        TextViewBirdInformation = findViewById(R.id.textViewResultBird);
+        TextViewInsectInformation = findViewById(R.id.textViewResultInsect);
+
+        GenerateSpecie();
+
+
+    }
+
+    private void GenerateSpecie() {
+        Random rnd = new Random();
+        catagory = rnd.nextInt(3);
+        if (catagory ==0){
+            index = rnd.nextInt(animals.length);
+            textView.setText(animals[index]);
+            answer = "animals";
+        } else if (catagory ==1){
+            index = rnd.nextInt(birds.length);
+            textView.setText(birds[index]);
+            answer = "birds";
+        }else if (catagory ==2){
+            index = rnd.nextInt(insects.length);
+            textView.setText(insects[index]);
+            answer = "insects";
+        }
+    }
 
 
 
